d9ce2defedc22d8087b411667322835fe2c0c231
kalmastenitin <nknckalmaste@gmail.com>
2023-05-12 20:32:38 +0530

    added flask with redis storage

--- flasksocket/run.py
+++ flasksocket/run.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
+import os
+from gevent import monkey
+monkey.patch_all()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app,logger=True, engineio_logger=True,cors_allowed_origins="*")
+socketio = SocketIO(app,
+                    logger=True,
+                    engineio_logger=True,
+                    cors_allowed_origins="*",
+                    message_queue='redis://:eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81@redis:6379/0')
 
 
 
@@ -17,7 +24,7 @@ def handle_json(json):
 
 @socketio.on('connect')
 def test_connect(auth):
-    emit('my response', {'data': 'Connected'})
+    emit('my response', {'data': 'Connected', "host":os.uname()[1]})
 
 @socketio.on('disconnect')
 def test_disconnect():
